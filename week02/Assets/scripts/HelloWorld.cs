a0f6b899ff67ea72370f1d57893af90731d90d29
Robert Yang <yang.robert.w@gmail.com>
2015-09-08 20:36:52 -0400

    added week02: intro to code

---
+++ week02/Assets/scripts/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	// code here will be executed once, at the very beginning of the game
+	// (like, right when we enter Play Mode) and never again
+	void Start () {
+		Debug.Log ("Hello World");
+	}
+	
+	// Update is called once per frame
+	// code here will happen all the time, constantly, forever
+	void Update () {
+		Debug.Log ("Bonjour Monde");
+	}
+}
---
+++ week02/Assets/scripts/PoopGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI; // lets us talk to Text objects
+
+public class PoopGame : MonoBehaviour {
+
+	// declare a variable to remember how many points the player has
+	float score = 0f; // declaring a decimal number that starts at zero
+
+	// we want this script to talk to our Text UI object?
+	// to do that, we have to tell Unity which Text object we want to talk to
+	public Text myText;
+	
+
+	// Update is called once per frame
+	void Update () {
+
+		// ask Unity if player is holding down Spacebar
+		if ( Input.GetKey (KeyCode.Space) ) {
+			// add "Time.deltaTime" on top of existing score
+			// "Time.deltaTime" = frame duration
+
+			// score = score + Time.deltaTime;
+			score += Time.deltaTime; // add points on top of existing score value
+			Debug.Log ( score.ToString() ); // print score to Console
+			myText.text = score.ToString (); // print score to UI
+		}
+	
+	}
+}

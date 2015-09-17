3e9c9c2665bda626c295917b08fbe5ec746db210
Robert Yang <yang.robert.w@gmail.com>
2015-09-17 17:05:44 -0400

    updated week3 with PlayerController.cs sample (for treasure hunt lab)

---
+++ week03/Assets/scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// I can tune this in the Inspector now
+	public float moveSpeed = 5f;
+
+	// IF YOU COPY THIS CODE VERBATIM IN YOUR ASSIGNMENT
+	// THEN WILL I WILL FIND OUT AND DESTROY YOU
+
+	// TYPE IT OUT YOURSELF AT LEAST OK
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey ( KeyCode.W ) ) {
+			transform.position += new Vector3( 0f, 0f, 1f ) * Time.deltaTime * moveSpeed;
+			transform.eulerAngles = new Vector3( 0f, 0f, 0f );
+			// transform.rotation is BAD AND SCARY
+		}
+
+		if ( Input.GetKey ( KeyCode.A ) ) {
+			transform.position += new Vector3( -1f, 0f, 0f ) * Time.deltaTime * moveSpeed;
+			transform.eulerAngles = new Vector3( 0f, -90f, 0f );
+		}
+
+		if ( Input.GetKey ( KeyCode.S ) ) {
+			transform.position += new Vector3( 0f, 0f, -1f ) * Time.deltaTime * moveSpeed;
+			transform.eulerAngles = new Vector3( 0f, 180f, 0f );
+		}
+
+		if ( Input.GetKey ( KeyCode.D ) ) {
+			transform.position += new Vector3( 1f, 0f, 0f ) * Time.deltaTime * moveSpeed;
+			transform.eulerAngles = new Vector3( 0f, 90f, 0f );
+		}
+
+		Camera.main.transform.position = transform.position + new Vector3(0f, 15f, -10f);
+	}
+}
---
+++ week03/Assets/scripts/VectorTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VectorTest : MonoBehaviour {
+
+	public Transform sphere; // assign in Inspector
+	public Transform cube; // assign in Inspector
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey (KeyCode.U ) ) {
+			cube.position += new Vector3(0f, 1f, 0f);
+		}
+
+		Debug.DrawLine ( sphere.position, cube.position, new Color( 1f, 0.5f, 0.1f) );
+
+		Debug.DrawLine ( Vector3.zero, new Vector3(1f, 5f, 2f) );
+		Debug.DrawLine ( new Vector3(1f, 5f, 2f), Vector3.one );
+	}
+}

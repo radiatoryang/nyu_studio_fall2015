5ffd9d7c1f823985de64ead41e43b0a30b0d9641
Robert Yang <yang.robert.w@gmail.com>
2015-09-22 20:33:45 -0400

    week4: review Treasure hunt vector math code, started physics unit

---
+++ week04/Assets/scripts/TreasureHint.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class TreasureHint : MonoBehaviour {
+
+	public Transform goalObject;
+	public Transform playerObject;
+
+	public Text uiText;
+
+	bool didIWinYet = false;
+
+	// Update is called once per frame
+	void Update () {
+		if ( (playerObject.position - goalObject.position).magnitude > 50f ) {
+			uiText.text = "you're going really far away now, this is bad";
+		} 
+
+		if ( Vector3.Distance ( goalObject.position, playerObject.position ) < 20f ) {
+			uiText.text = "you're getting closer!!!!";
+		}
+
+		if ( Vector3.Distance ( goalObject.position, playerObject.position ) < 5f ) {
+			uiText.text = "press SPACE TO WIN";
+			if ( Input.GetKeyDown (KeyCode.Space) ) {
+				didIWinYet = true;
+			}
+		}
+
+		if (didIWinYet) {
+			uiText.text = "you won";
+		}
+	
+	}
+}

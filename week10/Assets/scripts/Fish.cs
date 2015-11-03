aadd687757703437357769175e2fa9a6a9400093
Robert Yang <yang.robert.w@gmail.com>
2015-11-03 18:57:50 -0500

    week10a: fishes, instantiating and controlling fish.cs / fishgod.cs with while() and foreach()

---
+++ week10/Assets/scripts/Fish.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+
+	public float swimSpeed = 5f;
+	Vector3 destination; // position in world to swim towards
+
+	void Start () {
+		swimSpeed = Random.Range (1f, 5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// swim towards its destination
+		if ( Vector3.Distance ( transform.position, destination ) > 0.25f ) { // "stopping distance"
+			transform.position += (destination - transform.position).normalized * Time.deltaTime * swimSpeed;
+		} else { // if you reached your destination, set a new destination
+			SetDestination ( Random.insideUnitSphere * 100f ); 
+		}
+
+		// debug purposes, just to test the fish
+		if ( Input.GetKeyDown (KeyCode.D) ) {
+			// pick a random point inside an imaginary sphere of radius 10, centered at (0,0,0)
+			SetDestination ( Random.insideUnitSphere * 100f ); 
+		}
+	}
+
+	public void SetDestination (Vector3 newDestination) {
+		destination = newDestination;
+		transform.LookAt ( newDestination );
+	}
+}
---
+++ week10/Assets/scripts/FishGod.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic; // you need to add this line to use Lists
+
+public class FishGod : MonoBehaviour {
+
+	public Fish fishPrefab; // the fish prefab we clone
+	List<Fish> allTheFish = new List<Fish>(); // list of all the fish clones
+
+	// Use this for initialization
+	void Start () {
+		int fishCount = 0; // remember how many fish we have made
+		while ( fishCount < 100 ) {
+			Fish newFishClone = (Fish)Instantiate ( fishPrefab, Random.insideUnitSphere * 100f, Random.rotation );
+			newFishClone.swimSpeed = Random.Range (1f, 5f);
+			newFishClone.transform.localScale *= Random.Range (1f, 5f);
+			newFishClone.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1f);
+			allTheFish.Add ( newFishClone ); // adds the fish to the list
+			fishCount++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if you press space, make all the fish converge on (25f, 13f, 17f)
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			foreach ( Fish fish in allTheFish ) {
+				fish.SetDestination ( new Vector3(25f, 13f, 17f) );
+			}
+		}
+
+		// mouse cursor raycast
+		Ray ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
+		RaycastHit rayHit = new RaycastHit();
+		if ( Physics.Raycast ( ray, out rayHit, 100f ) ) {
+			// use a foreach loop to SetDestination to the RaycastHit point
+			foreach ( Fish fish in allTheFish ) {
+				fish.SetDestination ( rayHit.point );
+			}
+		}
+	}
+}

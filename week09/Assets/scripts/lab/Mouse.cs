64ac0d04d47dfc36ee1d3aaea16680cf95c9b27c
Robert Yang <yang.robert.w@gmail.com>
2015-11-03 18:57:19 -0500

    week09b: added Movement / Cat / Mouse steering AI examples from lab

---
+++ week09/Assets/scripts/lab/Mouse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+	//declare a public variable, of type Transform, called "cat"
+	public Transform cat;
+
+	void FixedUpdate () {
+		// declare a var of type Vector3, called "directionToCat", set to a vector that goes from [current position] to [cat's current position]
+		Vector3 directionToCat = cat.position - transform.position;
+
+		// if the angle between [current forward direction] vs. [directionToCat] is less than 180 degrees, then...
+		if ( Vector3.Angle ( transform.forward, directionToCat ) < 180f ) {
+			// declare a var of type Ray, called "mouseRay" that starts from [current position] and goes toward [cat's current position]
+			Ray mouseRay = new Ray( transform.position, directionToCat );
+			// declare a var of type RaycastHit, called "mouseRayHitInfo"
+			RaycastHit mouseRayHitInfo = new RaycastHit();
+			// if raycast with mouseRay and mouseRayHitInfo for 100 units is TRUE, then...
+			if ( Physics.Raycast ( mouseRay, out mouseRayHitInfo, 100f ) ) {
+				// if mouseRayHitInfo.collider.tag is exactly equal to the word "Cat"...
+				if ( mouseRayHitInfo.collider.tag == "Cat" ) {
+					Debug.DrawRay ( mouseRay.origin, mouseRay.direction * 100f, Color.cyan );
+					// run away from the cat!
+					GetComponent<Rigidbody>().AddForce ( (-directionToCat.normalized-transform.forward) * 1000f);
+				}
+			}
+		}
+	}
+}
---
+++ week09/Assets/scripts/lab/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Add a physics force going forward (to always move forward)
+		GetComponent<Rigidbody>().velocity = transform.forward * 10f + Physics.gravity;
+
+		// declare a var of type Ray, called "moveRay" that starts from [current position] and goes [current forward direction]
+		Ray ray = new Ray( transform.position, transform.forward );
+
+		// if Raycast with moveRay for 3 unit is TRUE...
+		if ( Physics.SphereCast ( ray, 1f, 3f ) ) {
+			// then randomly turn 90 degrees left or right (around Y axis)
+			transform.Rotate (0f, Random.value > 0.5 ? -90f : 90f, 0f );
+		}
+	}
+
+}

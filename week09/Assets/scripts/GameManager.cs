72ad9c5430160efb6d319605878ab5a4ac266749
Robert Yang <yang.robert.w@gmail.com>
2015-11-13 17:16:54 -0500

    week10: updated Cat and Mouse and GameManager.cs from week09

---
+++ week09/Assets/scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // lets me use Lists
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject catPrefab;
+	public GameObject mousePrefab;
+
+	public static List<GameObject> listOfCats = new List<GameObject>();
+	public static List<GameObject> listOfMouses = new List<GameObject>();
+
+	void Start () {
+		// reset manually
+		listOfCats.Clear ();
+		listOfMouses.Clear ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition );
+		RaycastHit rayHit = new RaycastHit();
+
+		if ( Physics.Raycast ( ray, out rayHit, 100f ) ) {
+			if ( Input.GetMouseButtonDown (0) ) {
+				GameObject newCat = (GameObject)Instantiate( catPrefab, rayHit.point, Quaternion.identity );
+				listOfCats.Add ( newCat );
+			}
+			if ( Input.GetMouseButtonDown (1) ) {
+				GameObject newMouse = (GameObject)Instantiate( mousePrefab, rayHit.point, Quaternion.identity );
+				listOfMouses.Add ( newMouse );
+			}
+		}
+	}
+}
--- week09/Assets/scripts/lab/Cat.cs
+++ week09/Assets/scripts/lab/Cat.cs
@@ -4,44 +4,48 @@ using System.Collections;
 public class Cat : MonoBehaviour {
 
 	// 	declare a public variable, of type Transform, called "mouse"
-	public Transform mouse;
+	// public Transform mouse;
 
 	bool shouldIPlayASound = false;
 
 	void FixedUpdate () {
-		//	declare a var of type Vector3, called "directionToMouse", set to a vector that goes from [current position] to [mouse's current position]
-		Vector3 directionToMouse = mouse.position - transform.position;
+		for ( int i=0; i<GameManager.listOfMouses.Count; i++ ) {
+			Transform mouse = GameManager.listOfMouses[i].transform;
 
-		// if the angle between [current forward direction] vs. [directionToMouse] is less than 90 degrees, then...
-		if ( Vector3.Angle ( transform.forward, directionToMouse ) < 90f ) {
-			//	declare a var of type Ray, called "catRay" that starts from [current position] and goes toward [mouse's current position]
-			Ray catRay = new Ray( transform.position, directionToMouse );
-			//	declare a var of type RaycastHit, called "catRayHitInfo"
-			RaycastHit catRayHitInfo = new RaycastHit();
-			//	if raycast with catRay and catRayHitInfo for 100 units is TRUE...
-			if ( Physics.Raycast ( catRay, out catRayHitInfo, 100f ) ) {
-				//	if catRayHitInfo.collider.tag is exactly equal to the word "Mouse"...
-				if ( catRayHitInfo.collider.tag == "Mouse" ) {
-					// the cat can see the mouse, play a sound
-					if ( GetComponent<AudioSource>().isPlaying == false && shouldIPlayASound == false ) {
-						GetComponent<AudioSource>().Play ();
-						shouldIPlayASound = true;
-					}
+			//	declare a var of type Vector3, called "directionToMouse", set to a vector that goes from [current position] to [mouse's current position]
+			Vector3 directionToMouse = mouse.position - transform.position;
 
-					Debug.DrawRay ( catRay.origin, catRay.direction * 100f, Color.yellow );
-					// if catRayHitInfo.distance is less than or equal to 5...
-					if ( catRayHitInfo.distance <= 5f ) {
-						//	then destroy the mouse object (we caught the mouse!)
-						Destroy ( mouse.gameObject );
-						// for class demo:
-						Application.LoadLevel (0);
-					//	else...
+			// if the angle between [current forward direction] vs. [directionToMouse] is less than 90 degrees, then...
+			if ( Vector3.Angle ( transform.forward, directionToMouse ) < 90f ) {
+				//	declare a var of type Ray, called "catRay" that starts from [current position] and goes toward [mouse's current position]
+				Ray catRay = new Ray( transform.position, directionToMouse );
+				//	declare a var of type RaycastHit, called "catRayHitInfo"
+				RaycastHit catRayHitInfo = new RaycastHit();
+				//	if raycast with catRay and catRayHitInfo for 100 units is TRUE...
+				if ( Physics.Raycast ( catRay, out catRayHitInfo, 100f ) ) {
+					//	if catRayHitInfo.collider.tag is exactly equal to the word "Mouse"...
+					if ( catRayHitInfo.collider.tag == "Mouse" ) {
+						// the cat can see the mouse, play a sound
+						if ( GetComponent<AudioSource>().isPlaying == false && shouldIPlayASound == false ) {
+							GetComponent<AudioSource>().Play ();
+							shouldIPlayASound = true;
+						}
+
+						Debug.DrawRay ( catRay.origin, catRay.direction * 100f, Color.yellow );
+						// if catRayHitInfo.distance is less than or equal to 5...
+						if ( catRayHitInfo.distance <= 5f ) {
+							//	then destroy the mouse object (we caught the mouse!)
+							Destroy ( mouse.gameObject );
+							// for class demo:
+							Application.LoadLevel (0);
+						//	else...
+						} else {
+							//	move toward the mouse!
+							GetComponent<Rigidbody>().AddForce (directionToMouse.normalized * 1000f);
+						}
 					} else {
-						//	move toward the mouse!
-						GetComponent<Rigidbody>().AddForce (directionToMouse.normalized * 1000f);
+						shouldIPlayASound = false; // reset sound variable
 					}
-				} else {
-					shouldIPlayASound = false; // reset sound variable
 				}
 			}
 		}

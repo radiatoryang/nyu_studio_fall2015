ca8c7c5277ee11b0ce73be167bea095b995f01e8
Robert Yang <yang.robert.w@gmail.com>
2015-12-02 12:26:52 -0500

    week14a: intro to coroutines, CoroutineDemoScene / BallSwap.cs

---
+++ week14/Assets/scripts/BallSwap.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSwap : MonoBehaviour {
+
+	public Transform ball1, ball2; // assign in inspector
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ( BallSwapCoroutine() );
+	}
+	
+	IEnumerator BallSwapCoroutine() {
+		while (true) { // infinite loops are OK in a coroutine
+			// remember where the balls started
+			Vector3 ball1Start = ball1.position;
+			Vector3 ball2Start = ball2.position;
+
+			bool didIPlayTheSoundAlready = false;
+			float t = 0f; // counter variable, "t" = "time"
+			while (t<1f) {
+				t += Time.deltaTime; // make t bigger
+				ball1.position = Vector3.Lerp (ball1Start, ball2Start, t);
+				ball2.position = Vector3.Lerp (ball2Start, ball1Start, t);
+				if ( t > 0.45f && t < 0.55f && didIPlayTheSoundAlready == false) {
+					Debug.Log ("play the sound!");
+					GetComponent<AudioSource>().Play (); // play bump sound
+					didIPlayTheSoundAlready = true;
+				}
+				yield return 0; // wait a frame
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
---
+++ week14/Assets/scripts/CoroutineDemo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine( MyFirstCoroutine() );
+	}
+	
+	IEnumerator MyFirstCoroutine () {
+		Debug.Log ("coroutine started!");
+		// a "yield" tells Unity to pause in this function
+		yield return 0; // wait for 1 frame
+		Debug.Log ("ok, I waited for 1 frame, now I'm continuing!...");
+		yield return null;
+		yield return 0;
+		Debug.Log ("ok I waited 2 more frames");
+		yield return new WaitForSeconds(1f); // wait 1 second
+		Debug.Log ("I'm done waiting!");
+
+	}
+
+}
